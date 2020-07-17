70c536b8d1d1b1588a5150b7ebb1772f83146d7a
Yashas10Gowda <44121735+Yashas10Gowda@users.noreply.github.com>
2020-07-17 13:47:47 +0530

    Need to implement timing function.

---
+++ DinoAutoPlay.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+var space = new KeyboardEvent('keydown', { 'keyCode': 32, 'which': 32 });
+var downdown = new KeyboardEvent('keydown', { 'keyCode': 40, 'which': 40 });
+var downup = new KeyboardEvent('keyup', { 'keyCode': 40, 'which': 40 });
+document.dispatchEvent(space);
+
+function jumpobstruction() {
+    for (let i = 0; i < 10; i++) {
+        if (ctx.getImageData(150 + i, 120, 1, 1).data[0] == 83) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function duckobstruction() {
+    for (let i = 0; i < 10; i++) {
+        if (ctx.getImageData(150 + i, 90, 1, 1).data[0] == 83) {
+            document.dispatchEvent(downdown);
+            setTimeout(function () {
+                document.dispatchEvent(downup);
+            }, 350);
+            break;
+        }
+    }
+}
+
+setInterval(() => {
+    jumpobstruction() ? document.dispatchEvent(space) : null;
+    duckobstruction();
+}, 10);
